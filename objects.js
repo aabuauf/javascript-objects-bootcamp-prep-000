376a0d510fc3bab4d95a9486df85356d62970064
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-15 11:20:46 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = new Object({Song:"Pair"});
 
+<<<<<<< HEAD
 function updatePlaylist(playlist, artistName, songTitle){
   return Object.assign(playlist,{[artistName]:songTitle})
 }
@@ -9,3 +10,5 @@ function removeFromPlaylist(playlist, artistName){
    delete playlist[artistName]
    return playlist
 }
+=======
+>>>>>>> 3ed90c8e45c152ef8c3030a7fad4302024636394
